e2b32c52c20c5fb5b8c1ae56d534d868908183ad
mhmdyafie <114470899+mhmdyafie@users.noreply.github.com>
2023-02-28 18:48:31 +0700

    week 1

---
+++ prakweb2/week1/test.php
@@ -0,0 +1,17 @@
+<?php
+
+// ==================================
+// definisikan variable
+$nama = 'Yafie';
+$umur = 18;
+$berat = 55;
+
+echo 'Nama : ' .$nama;
+echo '<br/>Umur : ' .$umur. ' Tahun ';
+echo '<br/>Berat :' .$berat. ' kg ';
+
+echo "<hr>";
+
+echo "<br/>  Hello $nama Apakabar?";
+echo "<br/> Hallo Kabar saya baik";
+?>
